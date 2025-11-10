b96bd0a240cc9a10873bc848cf9640d27be88463
GamerJacob <jacob.steinthal@gmail.com>
2025-11-10 12:44:46 +0100

    Push

--- intrusion_detec.cpp
+++ intrusion_detec.cpp
@@ -1,14 +1,38 @@
 #include <stdio.h>
+#include <iostream>
 
-
-bool intrusion_detection(int sensor_data[2], int camera_data[9][9]){
-    int sum=0;
-    for(int i=0;i<9;i++){
-        for(int j=0;i<9;i++){
-            sum += (sensor_data[0]+sensor_data[1])*camera_data[i][j];
+bool intrusion_detection(int sensor_data[2], int camera_data[9][9])
+{
+    int sum = 0;
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            sum += (sensor_data[0] + sensor_data[1]) * camera_data[i][j];
         }
     }
-    std::cout << "Sum is" << sum << std::endl;
-    return sum > 1900? true : false;
- 
+    std::cout << "Sum is: " << sum << std::endl;
+    return sum > 1900 ? true : false;
 }
+
+// int main()
+// {
+
+//     int signal[2] = {3, 4};
+//     int camera_data[9][9] = {
+//         {5, 5, 5, 5, 5, 5, 5, 5, 5},
+//         {5, 5, 5, 5, 5, 5, 5, 5, 5},
+//         {5, 5, 5, 5, 5, 5, 5, 5, 5},
+//         {5, 5, 5, 5, 5, 5, 5, 5, 5},
+//         {5, 5, 5, 5, 5, 5, 5, 5, 5},
+//         {5, 5, 5, 5, 5, 5, 5, 5, 5},
+//         {5, 5, 5, 5, 5, 5, 5, 5, 5},
+//         {5, 5, 5, 5, 5, 5, 5, 5, 5},
+//         {5, 5, 5, 5, 5, 5, 5, 5, 5}};
+
+//     if (intrusion_detection(signal, camera_data))
+//     {
+//         std::cout << "Intrusion detected!" << std::endl;
+//     }
+//     return 0;
+// }
--- main.cpp
+++ main.cpp
@@ -1,74 +1,81 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <iostream>  
-#include <chrono>
+#include <iostream>
+#include <cstdlib>
 #include <ctime>
+#include <chrono>
 #include <thread>
 #include "intrusion_detec.cpp"
 
-enum class alarm_type{
+enum class alarm_type
+{
     inactive,
     active,
     alarmed
 };
-using clock = std::chrono::steady_clock;
 
-//Random Pin generator between 0-num
-int get_pin(int num){
+using steady_clock_type = std::chrono::steady_clock;
+
+int get_pin(int num)
+{
     return rand() % num;
 }
 
-int main(){
-   
-    alarm_type alarm = alarm_type::inactive; 
+int main()
+{
+    srand(static_cast<unsigned>(time(0)));
 
-    // Something that checks for the valid P
-    while(alarm == alarm_type::inactive){
+    alarm_type alarm = alarm_type::inactive;
+
+    // Wait for valid PIN
+    while (alarm == alarm_type::inactive)
+    {
         int pin = get_pin(10);
-        pin % 2 == 0? alarm = alarm_type::active : alarm = alarm_type::inactive; 
-            std::cout << "Pincode is: "<< pin << std::endl;
+        alarm = (pin % 2 == 0) ? alarm_type::active : alarm_type::inactive;
+        std::cout << "Pincode is: " << pin << std::endl;
     }
-   std::cout << "System is active " << std::endl;
-    
-    
 
-    while(true){
-        int signal[2]={0}; 
+    std::cout << "System is active" << std::endl;
+
+    while (true)
+    {
+        int signal[2] = {0};
         signal[0] = get_pin(6);
 
-        // ----------time.sleep(60ms)----------
         std::this_thread::sleep_for(std::chrono::milliseconds(60));
 
-
         signal[1] = get_pin(6);
+
         int camera_data[9][9] = {{0}};
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
                 camera_data[i][j] = get_pin(10);
             }
         }
-        if (intrusion_detection(signal, camera_data)){
+
+        if (intrusion_detection(signal, camera_data))
+        {
             std::cout << "Intrusion detected!" << std::endl;
             alarm = alarm_type::alarmed;
-            
-            auto start = clock::now();
+
+            auto start = steady_clock_type::now();
             while (alarm == alarm_type::alarmed)
             {
-                // Break if pin is correct
-                get_pin(10) % 2 == 0? alarm = alarm_type::active : alarm = alarm_type::alarmed;
+                alarm = (get_pin(10) % 2 == 0) ? alarm_type::active : alarm_type::alarmed;
 
-                //Break if after 10 seconds
-                if(clock::now() - start > std::chrono::seconds(10)){
+                if (steady_clock_type::now() - start > std::chrono::seconds(10))
+                {
                     alarm = alarm_type::inactive;
                 }
-                
 
+                std::this_thread::sleep_for(std::chrono::milliseconds(50));
             }
+
             std::cout << "Alarm deactivated" << std::endl;
         }
-        
-        // ----------time.sleep(60ms)----------
+
         std::this_thread::sleep_for(std::chrono::milliseconds(60));
     }
+
     return 0;
 }
